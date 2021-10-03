31d4c10c35d0b73d9d0b46f3600bf8e484b1fc6d
Mridhodnick <mridhodnick11@gmail.com>
2021-10-03 11:20:37 +0700

    Grid System

--- resources/views/layouts/admin-template.blade.php
+++ resources/views/layouts/admin-template.blade.php
@@ -363,10 +363,51 @@
                 <!-- End of Topbar -->
 
                 <!-- Begin Page Content -->
-                <div class="container-fluid">
-                    @yield('content')
-                </div>
-
+                <div class="container">
+                    <div class="p-2"><h2>Grid 1</h2></div>
+                    <div class="row text-center justify-content-start">
+                      <div class="p-1 col-1 bg-primary text-white rounded">Kolom 1</div>
+                      <div class="p-1 col-1 bg-secondary text-white rounded">Kolom 2</div>
+                      <div class="p-1 col-1 bg-success text-white rounded">Kolom 3</div>
+                      <div class="p-1 col-1 bg-danger text-white rounded">Kolom 4</div>
+                    </div>
+                    <div class="p-2"><h2>Grid 2</h2></div>
+                    <div class="row justify-content-end">
+                        <div class="p-1 col-1 bg-primary text-white rounded">Kolom 1</div>
+                        <div class="p-1 col-1 bg-secondary text-white rounded">Kolom 2</div>
+                        <div class="p-1 col-1 bg-success text-white rounded">Kolom 3</div>
+                        <div class="p-1 col-1 bg-danger text-white rounded">Kolom 4</div>
+                    </div>
+                    <div class="p-2"><h2>Grid 3</h2></div>
+                    <div class="row justify-content-center">
+                        <div class="p-1 col-1 bg-primary text-white rounded">Kolom 1</div>
+                        <div class="p-1 col-1 bg-secondary text-white rounded">Kolom 2</div>
+                        <div class="p-1 col-1 bg-success text-white rounded">Kolom 3</div>
+                        <div class="p-1 col-1 bg-danger text-white rounded">Kolom 4</div>
+                    </div>
+                    <div class="p-2"><h2>Grid 4</h2></div>
+                    <div class="row justify-content-between">
+                        <div class="p-1 col-1 bg-primary text-white rounded">Kolom 1</div>
+                        <div class="p-1 col-1 bg-secondary text-white rounded">Kolom 2</div>
+                        <div class="p-1 col-1 bg-success text-white rounded">Kolom 3</div>
+                        <div class="p-1 col-1 bg-danger text-white rounded">Kolom 4</div>
+                    </div>
+                    <div class="p-2"><h2>Grid 5</h2></div>
+                    <div class="row justify-content-around">
+                        <div class="p-1 col-1 bg-primary text-white rounded">Kolom 1</div>
+                        <div class="p-1 col-1 bg-secondary text-white rounded">Kolom 2</div>
+                        <div class="p-1 col-1 bg-success text-white rounded">Kolom 3</div>
+                        <div class="p-1 col-1 bg-danger text-white rounded">Kolom 4</div>
+                    </div>
+                    <div class="p-2"><h2>Grid 7</h2></div>
+                    <div class="row justify-content-around">
+                        <div class="p-1 col-1 bg-primary text-white rounded">Kolom 1</div>
+                        <div class="p-1 col-2 bg-secondary text-white rounded">Kolom 2</div>
+                        <div class="p-1 col-3 bg-success text-white rounded">Kolom 3</div>
+                        <div class="p-1 col-4 bg-danger text-white rounded">Kolom 4</div>
+                    </div>
+                  </div>
+                
                 <!-- /.container-fluid -->
 
             </div>
